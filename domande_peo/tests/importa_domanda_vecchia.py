70e961c71fec4b4e902a7ad816e34c624b51ca03
Giuseppe De Marco <giuseppe.demarco@unical.it>
2020-11-12 12:50:16 +0100

    Update importa_domanda_vecchia.py

--- domande_peo/tests/importa_domanda_vecchia.py
+++ domande_peo/tests/importa_domanda_vecchia.py
@@ -4,16 +4,12 @@ from domande_peo.models import *
 from gestione_peo.models import *
 
 
-def migra_domanda_vecchia(matricola='', debug=1):
+def migra_domanda_vecchia(domanda_vecchia_pk='', domanda_nuova_pk='', debug=1):
     bando_vecchio = Bando.objects.all()[2]
-    domanda_vecchia = DomandaBando.objects.get(bando=bando_vecchio,
-                                               dipendente__matricola=matricola)
+    domanda_vecchia = DomandaBando.objects.get(pk=domanda_vecchia_pk)
+    domanda_nuova = DomandaBando.objects.get(pk=domanda_nuova_pk)
 
-    bando_nuovo = Bando.objects.last()
-    domanda_nuova = DomandaBando.objects.get(bando=bando_nuovo,
-                                             dipendente__matricola=matricola)
-
-    for i in domanda_vecchia.modulodomandabando_set.filter(disabilita=False):
+    for i in domanda_vecchia.modulodomandabando_set.filter(disabilita__in=(False, None)):
         nd = i.__dict__
         del(nd['_state'])
         nd['domanda_bando_id'] = domanda_nuova.pk
@@ -26,7 +22,7 @@ def migra_domanda_vecchia(matricola='', debug=1):
 
 
         desc_ind_vecchio = DescrizioneIndicatore.objects.get(pk=nd['descrizione_indicatore_id'])
-        indicatore_ponderato = IndicatorePonderato.objects.get(bando=bando_nuovo,
+        indicatore_ponderato = IndicatorePonderato.objects.get(bando=domanda_nuova.bando,
                                                                id_code=desc_ind_vecchio.indicatore_ponderato.id_code)
         desc_ind_nuovo = DescrizioneIndicatore.objects.get(indicatore_ponderato=indicatore_ponderato,
                                                            id_code=desc_ind_vecchio.id_code)
@@ -47,3 +43,5 @@ def migra_domanda_vecchia(matricola='', debug=1):
             print()
         else:
             ModuloDomandaBando.objects.create(**nd)
+
+migra_domanda_vecchia(domanda_nuova_pk=that_pk, domanda_vecchia_pk=that_pk, debug=1)
